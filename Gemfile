a9b493abc165bc4762262ef65a9c4e844b375847
Jyrki Pirtonen <jyrki.pirtonen@gmail.com>
2016-08-23 09:23:56 +0300

    Finish user signup

--- Gemfile
+++ Gemfile
@@ -58,6 +58,7 @@ group :test do
   gem 'capybara'
   gem 'guard-spork'
   gem 'spork'
+  gem 'factory_girl_rails'
 end
 
 group :production do
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
     
     before_save { |user| user.email = email.downcase }
     has_secure_password
-    
+ 
     validates :name, presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
